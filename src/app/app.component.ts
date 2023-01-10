b33b437c6e0ecad83952105a8b9f830de9780d0c
ketandeshmukh <deshmukhketan647@gmail.com>
2023-01-10 16:50:24 +0530

    "This is our code"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+           <app-team></app-team>
+          `
 })
 export class AppComponent {
-  title = 'onlyFormArray';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeamManagementComponent } from './team-management.component';
+import { TeamManagementService } from './team-management.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+      imports: [
+            BrowserModule,
+            ReactiveFormsModule
+      ],
+      declarations: [
+            AppComponent,
+            TeamManagementComponent
+      ],
+      providers: [
+            TeamManagementService
+      ],
+      bootstrap: [
+            AppComponent
+      ]
 })
 export class AppModule { }
